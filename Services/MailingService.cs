0c0153a26cd539066e3d669eed0e0b4c303603b3
AbdelrahmanMashhour <abdofamous458@gmail.com>
2024-01-30 16:56:37 +0200

    End First Vedio From Play List

---
+++ Services/MailingService.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.Options;
+using MimeKit;
+using SendingEmails.Settings;
+
+namespace SendingEmails.Services
+{
+    public class MailingService : IMailingService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public MailingService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string mailTo, string subject, string body, IList<IFormFile>? attachments)
+        {
+            var email = new MimeMessage
+            {
+                Sender=MailboxAddress.Parse(_mailSettings.Email)
+            };
+        }
+    }
+}
